2d6d89d628b1e3adecd766c05fa9020f52431150
sydneybucc <s_bucci@fanshaweonline.ca>
2018-04-22 14:17:52 -0400

    reverting back to old code

--- admin/admin_editall.php
+++ admin/admin_editall.php
@@ -1,6 +1,6 @@
 <?php
-    // ini_set('display_errors',1);
-    // error_reporting(E_ALL);
+    ini_set('display_errors',1);
+    error_reporting(E_ALL);
     require_once('phpscripts/config.php');
 ?>
 <!DOCTYPE html>
@@ -19,33 +19,8 @@
     <div class="large-6 large-offset-3 columns topSpace">
         <h2>Edit Movie</h2>
         <?php
-            // echo single_edit('tbl_movies','movies_id',1);
+            echo single_edit('tbl_movies','movies_id',1);
         ?>
-        
-        
-        
-        <?php if(!empty($message)){echo $message;} ?>
-		<form action="admin_editall.php" method="post">
-		<label class="formStyle">Title:</label>
-		<input type="text" name="title" value="<?php echo $found_user['movies_title'];?>"><br><br>
-		<label class="formStyle">Description</label>
-		<input type="text" name="desc" value="<?php echo $found_user['movies_desc'];?>"><br><br>
-		<label class="formStyle">Cover:</label>
-		<input type="text" name="cover" value="<?php echo $found_user['movies_cover'];?>"><br><br>
-		<label class="formStyle">Trailer:</label>
-        <input type="text" name="trailer" value="<?php echo $found_user['movies_trailer'];?>"><br><br>
-        <label class="formStyle">Runtime:</label>
-        <input type="text" name="runtime" value="<?php echo $found_user['movies_runtime'];?>"><br><br>
-        <label class="formStyle">Release:</label>
-        <input type="text" name="release" value="<?php echo $found_user['movies_release'];?>"><br><br>
-        <label class="formStyle">Rating:</label>
-		<input type="text" name="rating" value="<?php echo $found_user['movies_rating'];?>"><br><br>
-		<br><br>
-        <input type ="submit" name="submit" value="Edit User" class="send">
-        
-
-
-		</form>
     </div>
 </div>
 </body>
