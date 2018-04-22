420d5f74d16c749634d2561190872fcc79009c9b
sydneybucc <s_bucci@fanshaweonline.ca>
2018-04-21 23:43:31 -0400

    styling edit user page

--- admin/admin_edituser.php
+++ admin/admin_edituser.php
@@ -28,27 +28,30 @@
 <head>
 <meta charset="UTF-8">
 <link href="https://fonts.googleapis.com/css?family=Anton|Lato" rel="stylesheet">
+<link rel="stylesheet" href="css/foundation.css">
 <link href="css/admin_main.css" rel="stylesheet" type="text/css" media="screen">
 <title>CMS Portal Login</title>
 </head>
 <body>
-<div class="center">
-	<h2>Edit User</h2>
-	<?php if(!empty($message)){echo $message;} ?>
-	<form action="admin_edituser.php" method="post">
-	<label class="formStyle">First Name:</label>
-	<input type="text" name="fname" value="<?php echo $found_user['user_fname'];?>"<br><br>
-	<label class="formStyle">Username:</label>
-	<input type="text" name="username" value="<?php echo $found_user['user_name'];?>"><br><br>
-	<label class="formStyle">Password:</label>
-	<input type="text" name="password" value="<?php echo $found_user['user_pass'];?>"><br><br>
-	<label class="formStyle">Email:</label>
-	<input type="text" name="email" value="<?php echo $found_user['user_email'];?>"><br><br>
-	<br><br>
-	<input type ="submit" name="submit" value="Edit User" class="send">
+<div class="expanded row">
+	<div class="large-6 large-offset-3 columns topSpace">
+		<h2>Edit User</h2>
+		<?php if(!empty($message)){echo $message;} ?>
+		<form action="admin_edituser.php" method="post">
+		<label class="formStyle">First Name:</label>
+		<input type="text" name="fname" value="<?php echo $found_user['user_fname'];?>"<br><br>
+		<label class="formStyle">Username:</label>
+		<input type="text" name="username" value="<?php echo $found_user['user_name'];?>"><br><br>
+		<label class="formStyle">Password:</label>
+		<input type="text" name="password" value="<?php echo $found_user['user_pass'];?>"><br><br>
+		<label class="formStyle">Email:</label>
+		<input type="text" name="email" value="<?php echo $found_user['user_email'];?>"><br><br>
+		<br><br>
+		<input type ="submit" name="submit" value="Edit User" class="send">
 
 
-	</form>
+		</form>
+	</div>
 </div>
 </body>
 </html>
